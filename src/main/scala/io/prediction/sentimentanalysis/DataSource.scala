c32f1d3165aa8a2f533a8f7ff18f9ca0529cc152
Todd Maegerle <toddis@uw.edu>
2015-05-14 00:38:25 -0700

    Builds. Need to change logic in calculation method.

--- src/main/scala/io/prediction/sentimentanalysis/DataSource.scala
+++ src/main/scala/io/prediction/sentimentanalysis/DataSource.scala
@@ -1,16 +1,74 @@
 package io.prediction.sentimentanalysis
 
-import io.prediction.controller.{EmptyActualResult, EmptyEvaluationInfo, PDataSource}
+import io.prediction.controller.{EmptyEvaluationInfo, PDataSource}
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
+
+import io.prediction.data.store.PEventStore
+
+import io.prediction.sentimentanalysis.ActualResult
+
+import grizzled.slf4j.Logger
 
 class DataSource(val dsp: DataSourceParams) extends PDataSource[
   TrainingData,
   EmptyEvaluationInfo,
   Query,
-  EmptyActualResult] {
+  ActualResult] {
+  
+  @transient lazy val logger = Logger[this.type]
 
   override def readTraining(sc: SparkContext): TrainingData = {
     TrainingData()
   }
+  
+  override
+  def readEval(sc: SparkContext)
+  : Seq[(TrainingData, EmptyEvaluationInfo, RDD[(Query, ActualResult)])] = {
+
+    // The following code reads the data from data store. It is equivalent to
+    // the readTraining method. We copy-and-paste the exact code here for
+    // illustration purpose, a recommended approach is to factor out this logic
+    // into a helper function and have both readTraining and readEval call the
+    // helper.
+    val labeledPoints: RDD[TweetSentiment] = PEventStore.aggregateProperties(
+      appName = "Tweet Sentiment Analysis",
+      entityType = "user",
+      // only keep entities with these required properties defined
+      required = Some(List("tweet", "sentiment")))(sc)
+      // aggregateProperties() returns RDD pair of
+      // entity ID and its aggregated properties
+      .map { case (entityId, properties) =>
+        try {
+          new TweetSentiment(properties.get[String]("tweet"),
+            properties.get[Int]("sentiment")
+          )
+        } catch {
+          case e: Exception => {
+            logger.error(s"Failed to get properties ${properties} of" +
+              s" ${entityId}. Exception: ${e}.")
+            throw e
+          }
+        }
+      }.cache()
+      // End of reading from data store
+
+    val indexedPoints: RDD[(TweetSentiment, Long)] = labeledPoints.zipWithIndex()
+    val evalK = indexedPoints.count().toInt
+    (0 until evalK).map { idx =>
+      val trainingPoints = indexedPoints.filter(_._2 % evalK == idx).map(_._1)
+      val testingPoints = indexedPoints.filter(_._2 % evalK != idx).map(_._1)
+
+      (
+        TrainingData(),
+        new EmptyEvaluationInfo(),
+        testingPoints.map {
+          p => (new Query(p.tweet), new ActualResult(p.sentiment))
+        }
+      )
+    }
+  }
 }
+
+class TweetSentiment(val tweet: String, val sentiment: Int) extends Serializable
+
--- src/main/scala/io/prediction/sentimentanalysis/Evaluation.scala
+++ src/main/scala/io/prediction/sentimentanalysis/Evaluation.scala
@@ -1,4 +1,4 @@
-package org.template.classification
+package io.prediction.sentimentanalysis
 
 import io.prediction.controller.AverageMetric
 import io.prediction.controller.EmptyEvaluationInfo
@@ -11,6 +11,8 @@ import io.prediction.sentimentanalysis.PredictedResult
 import io.prediction.sentimentanalysis.ActualResult
 import io.prediction.sentimentanalysis.Query
 import io.prediction.sentimentanalysis.EngineFactory
+import io.prediction.sentimentanalysis.DataSourceParams
+import io.prediction.sentimentanalysis.AlgorithmParams
 
 case class Precision(label: Double)
   extends OptionAverageMetric[EmptyEvaluationInfo, Query, PredictedResult, ActualResult] {
@@ -30,6 +32,23 @@ case class Precision(label: Double)
 
 object PrecisionEvaluation extends Evaluation {
   engineMetric = (EngineFactory(), new Precision(label = 1.0))
+}
+
+object EngineParamsList extends EngineParamsGenerator {
+  // Define list of EngineParams used in Evaluation
+
+  // First, we define the base engine params. It specifies the appId from which
+  // the data is read, and a evalK parameter is used to define the
+  // cross-validation.
+  private[this] val baseEP = EngineParams(
+    dataSourceParams = DataSourceParams())
+
+  // Second, we specify the engine params list by explicitly listing all
+  // algorithm parameters. In this case, we evaluate 3 engine params, each with
+  // a different algorithm params value.
+  engineParamsList = Seq(
+    baseEP.copy(algorithmParamsList = Seq(("algo", AlgorithmParams("./data/englishPCFG.ser.gz", "./data/sentiment.ser.gz"))))
+   )
 }
 
 
